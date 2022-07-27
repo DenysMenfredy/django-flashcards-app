6ae67db1b3f599fe90c9f5bc4a5c97e92011973b
DenysMenfredy <denys.menfredy7@gmail.com>
2022-07-27 11:02:10 -0300

    checking answers and moving cards between boxes

--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from random import choice
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Card
+from .forms import CardCheckForm
 # Create your views here.
 
 
@@ -16,3 +18,25 @@ class CardCreateView(CreateView):
 
 class CardUpdateView(CardCreateView, UpdateView):
     success_url = reverse_lazy("card-list")
+
+class BoxView(ListView):
+    template_name: str = "cards/box.html"
+    form_class = CardCheckForm
+
+    def get_queryset(self):
+        return Card.objects.filter(box=self.kwargs["box_num"])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = choice(self.object_list)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+            card.move(form.cleaned_data["solved"])
+
+        return redirect(request.META.get("HTTP_REFERER"))
