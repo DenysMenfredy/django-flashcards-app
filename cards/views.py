d2a0c150fc8692627c813180751ead5535044180
DenysMenfredy <denys.menfredy7@gmail.com>
2022-07-27 01:17:05 -0300

    creating card using form, updating form

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Card
 # Create your views here.
 
@@ -7,3 +8,11 @@ from .models import Card
 class CardListView(ListView):
     model = Card
     queryset = Card.objects.all().order_by("box", "date_created")
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy("card-list")
